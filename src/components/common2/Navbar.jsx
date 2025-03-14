2d3009ac0ed86198b8975bcfa65ad789891bde02
sashi17 <sashikantapattanaik2005@gmail.com>
2025-03-14 23:51:35 +0530

    navbar2

--- src/components/common2/Navbar.jsx
+++ src/components/common2/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import logo from "../../assets/Logo/Logo-Full-Light.png"
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
     <div className='flex '>
         <div className='flex w-11/12 max-w-maxContent items-center justify-between'>
-
+          <Link>
+            <img src={logo} height={42} width={160} loading='lazy' alt="alt" />
+          </Link>
         </div>
     </div>
  )
